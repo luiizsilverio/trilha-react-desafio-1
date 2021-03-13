5db94d804d367046a34cac247a99f9cda38a1274
luiizsilverio <luiiz.silverio@gmail.com>
2021-03-12 22:05:20 -0300

    resolução do desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -36,18 +36,29 @@ export function TaskList() {
         isComplete: false,
       }
       
-      setTasks([...tasks, newTask])
+      //setTasks([...tasks, newTask])
+      setTasks(oldState => [...oldState, newTask])
+      setNewTaskTitle('')
     }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+/*    
     const indice = tasks.findIndex(task => task.id === id)
 
     if (indice >= 0) {
       tasks[indice].isComplete = !tasks[indice].isComplete      
       setTasks([...tasks])
     }
+*/
+    const newTasks = tasks.map( task => 
+      task.id === id ?
+      { ...task, isComplete: !task.isComplete }
+      : task
+    )
+
+    setTasks(newTasks)    
   }
 
   function handleRemoveTask(id: number) {
